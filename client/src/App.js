d0483178f75e6bde112d0ec4f26ab87997ede58c
william12559 <144663775+william12559@users.noreply.github.com>
2025-11-04 12:58:55 +0100

    betaalpagina

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import ContactPage from "./ContactPage";
 import VerhaalPage from "./VerhaalPage";
+import InfomatiePage from "./InfomatiePage";
+import BetaalPage from "./BetaalPage";
 import AdminLoginPage from "./AdminLoginPage";
 import DashboardPage from "./DashboardPage";
 import PrivateAdminRoute from "./PrivateAdminRoute";
@@ -14,6 +16,8 @@ function App() {
         <Route path="/HomePage" element={<HomePage />} />`
         <Route path="/ContactPage" element={<ContactPage />} />
         <Route path="/VerhaalPage" element={<VerhaalPage />} />
+        <Route path="/BetaalPage" element={<BetaalPage />} />
+        <Route path="/InfomatiePage" element={<InfomatiePage />} />
         <Route path="/adminlogin" element={<AdminLoginPage />} />
         <Route
           path="/dashboard"
